595ef7d7affb7b1c927b4c869ae502a84c30d345
yar-web <yaroslavorel950@gmail.com>
2025-01-29 21:56:15 +0100

    first commit

---
+++ app/public/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ConvertTo</title>
+  <link rel="stylesheet" href="/css/components.css">
+  <link rel="stylesheet" href="/css/base.css">
+</head>
+<body>
+  <form action="handler.php" method="post" enctype="multipart/form-data">
+    <div class="form-logo">
+      <img src= "/images/ConvertTo.svg" class="form-logo__img" alt="logo">
+    </div>
+    <h3 class="form__title">Select a type and a format of files</h3>
+    <div>
+      <select class="form-select" name="" id="">
+        <option value=""></option>
+      </select>
+      <select class="form-select" name="" id="">
+        <option value=""></option>
+      </select>
+    </div>
+    <input type="file">
+  </form>
+</body>
+</html>
